6037d4bfb03ec87764a8de91df32c42a25fd853d
muhammad iqbal <iqbal.muh998@gmail.com>
2022-03-25 08:33:15 +0700

    commit masterr1

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const port = process.env.PORT || 3000;
-// const port = 3000
+// const port = process.env.PORT || 3000;
+const port = 3000
 const route = require('./routes/index')
 // app.set('trust proxy', 1)
 
